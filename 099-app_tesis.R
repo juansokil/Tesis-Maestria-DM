55117045fc25749df830c642418bc789304bcca0
Juan Sokil <juanpablosokil@gmail.com>
2018-10-25 17:02:24 -0300

    Update 099-app_tesis.R

--- 099-app_tesis.R
+++ 099-app_tesis.R
@@ -4,28 +4,20 @@ library(data.table)
 library(ggplot2)
 library(stringr)
 library(dplyr)
-
+library(here)
 
 ####### BASES ######
 
-#base <- fread('C:/Users/Juan/Dropbox/POSGRADOS/Uba/99-Tesis/scripts/scripts/resultados/base.txt', sep='\t', encoding='Latin-1')
-base <- fread('C:/Users/observatorio/Desktop/App_Tesis/App-Tesis/base/base.txt', sep='\t', encoding='Latin-1')
+
+base <- fread(here('/Scripts-Tesis/App_Tesis/base/base.txt'), sep='\t', encoding='Latin-1')
 base$topico <- str_sub(base$term, -1)
 
-
-
-
-
-#tsne <- fread('C:/Users/Juan/Dropbox/POSGRADOS/Uba/99-Tesis/scripts/scripts/resultados/datos_tsne_DTM1.csv', sep='\t')
-#tsne2 <- fread('C:/Users/Juan/Dropbox/POSGRADOS/Uba/99-Tesis/scripts/scripts/resultados/datos_tsne_DTM2.csv', sep='\t')
-#tsne3 <- fread('C:/Users/Juan/Dropbox/POSGRADOS/Uba/99-Tesis/scripts/scripts/resultados/datos_tsne_DTM3.csv', sep='\t')
-
-tsne <- fread('C:/Users/observatorio/Desktop/App_Tesis/App-Tesis/base/datos_tsne_DTM1.csv', sep='\t')
-tsne2 <- fread('C:/Users/observatorio/Desktop/App_Tesis/App-Tesis/base/datos_tsne_DTM2.csv', sep='\t')
-tsne3 <- fread('C:/Users/observatorio/Desktop/App_Tesis/App-Tesis/base/datos_tsne_DTM3.csv', sep='\t')
-tsne4 <- fread('C:/Users/observatorio/Desktop/App_Tesis/App-Tesis/base/datos_tsne_DTM4.csv', sep='\t')
-tsne5 <- fread('C:/Users/observatorio/Desktop/App_Tesis/App-Tesis/base/datos_tsne_DTM5.csv', sep='\t')
-tsne6 <- fread('C:/Users/observatorio/Desktop/App_Tesis/App-Tesis/base/datos_tsne_DTM6.csv', sep='\t')
+tsne <- fread(here('/Scripts-Tesis/App_Tesis/base/datos_tsne_DTM1.csv'), sep='\t')
+tsne2 <- fread(here('/Scripts-Tesis/App_Tesis/base/datos_tsne_DTM2.csv'), sep='\t')
+tsne3 <- fread(here('/Scripts-Tesis/App_Tesis/base/datos_tsne_DTM3.csv'), sep='\t')
+tsne4 <- fread(here('/Scripts-Tesis/App_Tesis/base/datos_tsne_DTM4.csv'), sep='\t')
+tsne5 <- fread(here('/Scripts-Tesis/App_Tesis/base/datos_tsne_DTM5.csv'), sep='\t')
+tsne6 <- fread(here('/Scripts-Tesis/App_Tesis/base/datos_tsne_DTM6.csv'), sep='\t')
 
 tsne$year <- 2003
 tsne2$year <- 2004
@@ -34,10 +26,11 @@ tsne4$year <- 2006
 tsne5$year <- 2007
 tsne6$year <- 2008
 
-
 base_topic <- rbind(tsne, tsne2, tsne3, tsne4, tsne5, tsne6)
 
-##RELEVANCE
+
+
+####RELEVANCE#####
 base_topic$prob <- base_topic$loglift*0.4+base_topic$logprob*0.6
 base_topic2 <- base_topic %>%
   select (Category,  Term, year, prob)
@@ -55,11 +48,8 @@ base_topic2 <- base_topic2 %>%
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   titlePanel("Aplicacion ejemplo"),
-  #sidebarLayout(sidebarPanel(sliderInput("bins","Year:",min = 2000,max = 2007, value=2000)),
   sidebarLayout(sidebarPanel(checkboxGroupInput("topico", "topico:", choices=unique(base$term)),
-                             sliderInput("year", "year:", min=2003, max=2008, value=2003, animate =TRUE)
-  ),
-  # Show a plot of the generated distribution
+                             sliderInput("year", "year:", min=2003, max=2008, value=2003, animate =TRUE)),
   mainPanel( fluidRow(
     column(2,
            plotOutput(outputId = "distPlot", width  = "300px",height = "200px"),  
@@ -67,14 +57,10 @@ ui <- fluidPage(
     fluidRow(plotOutput(outputId = "distPlot3", width  = "500px",height = "200px"))
   ))))
 
-# Define server logic required to draw a histogram
-#server <- function(input, output) {
+
 server <- function(input, output, session) {
   base2 <- reactive({
-    #a <- subset(base, term == "top1")
-    # You can also use the pipe (%>%) operator
     a <- base %>% filter(term %in% input$topico)
-    #  a <- subset(base, term == input$topico)
     a <- data.frame(a)
     a$year <- as.integer(a$year)
     return(a)
@@ -91,10 +77,7 @@ server <- function(input, output, session) {
   
   
   base3 <- reactive({
-    #a <- subset(base, term == "top1")
-    # You can also use the pipe (%>%) operator
     a <- base_topic2 %>% filter(term %in% input$topico)
-    #  a <- subset(base, term == input$topico)
     a <- data.frame(a)
     return(a)
     
@@ -125,7 +108,8 @@ server <- function(input, output, session) {
   output$distPlot3 <- renderPlot({
     ggplot(base4(), aes(x=word, y=prob, color=term, fill=term)) + 
       geom_bar(stat='identity') +
-      coord_flip() 
+      coord_flip()  +
+      theme(axis.title.y = element_blank(),legend.position="none") 
   })
   
 }
@@ -138,174 +122,4 @@ shinyApp(ui = ui, server = server)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-### LIBRERIAS###
-library(shiny)
-library(data.table)
-library(ggplot2)
-library(stringr)
-library(dplyr)
-
-
-####
-
-
-
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   titlePanel("Aplicacion ejemplo"),
-   #sidebarLayout(sidebarPanel(sliderInput("bins","Year:",min = 2000,max = 2007, value=2000)),
-   sidebarLayout(sidebarPanel(checkboxGroupInput("topico", "topico:", choices=unique(base$term))),
-      # Show a plot of the generated distribution
-      mainPanel(plotOutput("distPlot"))
-   )
-)
-
-
-
-# Define server logic required to draw a histogram
-#server <- function(input, output) {
-server <- function(input, output, session) {
-  
-  
-base2 <- reactive({
-    #a <- subset(base, term == "top1")
-  # You can also use the pipe (%>%) operator
-  a <- base %>% filter(term %in% input$topico)
-  #  a <- subset(base, term == input$topico)
-    a <- data.frame(a)
-    return(a)
-  
-   })
-
-
-   output$distPlot <- renderPlot({
-     ggplot(base2(), aes(year, prob, color=term)) + 
-       geom_line() +
-       scale_y_continuous(limits = c(0, 2)) + 
-       scale_x_continuous(limits = c(2000, 2007))
-   })
-}
-
-
-# Run the application 
-shinyApp(ui = ui, server = server)
 
