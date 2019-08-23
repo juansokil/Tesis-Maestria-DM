ed6d85b032e248163f6de8b59e5ed2072fe9be25
Juan Pablo Sokil <juanpablosokil@gmail.com>
2019-08-22 23:52:03 -0300

    Update Analisis_Topicos.R

--- Analisis_Topicos.R
+++ Analisis_Topicos.R
@@ -1,14 +1,16 @@
 library(readr)
 library(dplyr)
-base_completa <- read_delim("C:/Users/Juan/Dropbox/POSGRADOS/Uba/99-Tesis/scripts/scripts/LDA/base_completa.csv", ",", escape_double = TRUE, trim_ws = TRUE)
+
+base_completa <- read_delim("C:/Users/Juan/Dropbox/POSGRADOS/Uba/99-Tesis/scripts/scripts/LDA/base_completa.csv",   "\t", escape_double = FALSE, col_types = cols(X1 = col_skip(), id_1 = col_skip()), trim_ws = TRUE)
+
 
 glimpse(base_completa)
 
 
-table(base_completa$Año,base_completa$dominant_topic)
+table(base_completa$AÃ±o,base_completa$dominant_topic)
 
 bla <- base_completa %>%
-  group_by(Año) %>%
+  group_by(AÃ±o) %>%
   summarise_each(funs(mean(., na.rm = TRUE)))
   
 
