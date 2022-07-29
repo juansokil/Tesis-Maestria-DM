80bdd8bdaa6ab326f509f3157216627e753b589b
Juan Pablo Sokil <juanpablosokil@gmail.com>
2022-07-28 21:09:30 -0300

    Delete 00-shiny.R

--- base_app/00-shiny.R
+++
@@ -1,5 +0,0 @@
-####Conector Shiny###
-
-rsconnect::setAccountInfo(name='juanpablosokil', 
-                          token='7499F5689D7DC0540DB1D96DCC05DB0F', 
-                          secret='YanlwsVRMkrfX3dy3tAXnHttmNZh1lcXZME/IISR')
